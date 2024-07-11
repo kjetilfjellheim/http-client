b260e9a8a362ad29ea32477a4396c2338c816780
Kjetil Fjellheim <kjetil@forgottendonkey.net>
2024-07-12 00:26:06 +0200

    feat: Add urldecode function
    Add urldecode function to src/common/urlencode.rs
    
    Breaking changes: None
    
    Resolves: None

--- src/common/urlencode.rs
+++ src/common/urlencode.rs
@@ -1,4 +1,22 @@
 /**
+ * URL decoding error. This error is thrown when
+ * the URL encoded string can't be decoded to a normal
+ * string. The error contains a message with the reason.
+ */
+#[derive(Debug)]
+struct DecodeError {
+    message: String
+}
+
+impl DecodeError {
+    pub fn new(message: String) -> DecodeError {
+        DecodeError {
+            message
+        }
+    }
+}
+
+/**
  * Characters that don't need to be encoded. Im the URI 
  * standard, these are called "unreserved characters".
  * 
@@ -40,7 +58,6 @@ fn encode_char(character: &char) -> String {
     } else {
         let string = character.to_string();
         let bytes = string.as_bytes();
-        let mut result = String::new();
         encode_non_reserved_char(bytes)
     }
 }
@@ -62,13 +79,87 @@ fn encode_non_reserved_char(bytes: &[u8]) -> String {
     result
 }
 
+/**
+ * Decodes a URL encoded string to a normal string.
+ * 
+ * @param input The URL encoded string.
+ * 
+ * @return The normal string.
+ */
+pub fn decode(str: &str) -> Result<String, DecodeError> {
+    let mut result = String::new();
+    let mut chars = str.chars();
+    while let Some(character) = chars.next() {
+        if character == '%' {
+            let bytes = get_bytes(&mut chars)?;
+            let utf8 = match String::from_utf8(bytes) {
+                Ok(utf8) => utf8,
+                Err(err) => return Err(DecodeError::new(err.to_string()))
+            };
+            result.push_str(utf8.as_str());
+        } else {
+            result.push(character);
+        }
+    }
+    Ok(result)
+}
+
+/**
+ * Gets the bytes of a URL encoded character.
+ * 
+ * @param chars The iterator of characters.
+ * 
+ * @return The bytes of the URL encoded character.
+ */
+fn get_bytes(chars: &mut std::str::Chars) -> Result<Vec<u8>, DecodeError> {
+    let mut bytes: Vec<u8> = Vec::new();
+    let byte = get_byte(chars)?;
+    bytes.push(byte);
+    if byte >= 128 {
+        chars.next();
+        bytes.push(get_byte(chars)?);
+    }
+    Ok(bytes)
+}
+
+/**
+ * Gets a byte from the URL encoded string.
+ * 
+ * @param chars The iterator of characters.
+ * 
+ * @return The byte.
+ */
+fn get_byte(chars: &mut std::str::Chars) -> Result<u8,DecodeError> {
+    let char1 = match chars.next() {
+        Some(char) => char,
+        None => return Err(DecodeError::new("Unexpected end of string".to_string()))
+    };
+    let char2 = match chars.next() {
+        Some(char) => char,
+        None => return Err(DecodeError::new("Unexpected end of string".to_string()))
+    };
+    match u8::from_str_radix(format!("{}{}", char1, char2).as_str(), 16) {
+        Ok(byte) => Ok(byte),
+        Err(err) => Err(DecodeError::new(err.to_string()))
+    }
+}
+
 #[cfg(test)]
 mod test {
 
     #[test]
     fn test_encode() {
+        assert_eq!(super::encode("abcd"), "abcd");
         assert_eq!(super::encode("http://localhost:8080"), "http%3A%2F%2Flocalhost%3A8080");
         assert_eq!(super::encode("Hello Günter"), "Hello%20G%C3%BCnter");
         assert_eq!(super::encode("æøåÆØÅ,.-;:_!\"#¤%"), "%C3%A6%C3%B8%C3%A5%C3%86%C3%98%C3%85%2C.-%3B%3A_%21%22%23%C2%A4%25");
+    }
+
+    #[test]
+    fn test_decode() {
+        assert_eq!(super::decode("abcd").unwrap(), "abcd");
+        assert_eq!(super::decode("http%3A%2F%2Flocalhost%3A8080").unwrap(), "http://localhost:8080");
+        assert_eq!(super::decode("Hello%20G%C3%BCnter").unwrap(), "Hello Günter");
+        assert_eq!(super::decode("%C3%A6%C3%B8%C3%A5%C3%86%C3%98%C3%85%2C.-%3B%3A_%21%22%23%C2%A4%25").unwrap(), "æøåÆØÅ,.-;:_!\"#¤%");
     }
 }
